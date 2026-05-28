3601fab7beb0ae51087a2b336c86978dd70728ff
Sondre Haugom <sondrehaugom@gmail.com>
2026-05-28 14:22:17 +0200

    lagt inn skript for å kjøre direkte objekt gjenkjenning ved live kamera

--- yolo-objektdeteksjon/del1_grunnlegende/test_yolo.py
+++ yolo-objektdeteksjon/del1_grunnlegende/test_yolo.py
@@ -1,17 +1,18 @@
 from ultralytics import YOLO
 
-modell = YOLO("best.pt")          # laster ned automatisk første gang
+modell = YOLO("last.pt")          # laster ned automatisk første gang
 
 result = modell(
-    "yolo-objektdeteksjon/del1_grunnlegende/20260528_122934.jpg",
-    conf=0.70, # minimun confidence for å beholde deteksjonen
-    iou=0.30, # 
+    "yolo-objektdeteksjon/del1_grunnlegende/20260528_133629.jpg",
+    conf=0.50, # minimun confidence for å beholde deteksjonen
+    iou=0.10, # 
     max_det=10, # maks antall deteksjoner per bilde
     agnostic_nms=True, # ikke skille mellom klasser ved NMS
 )
 
 result[0].save("Resultat.jpg") # lagrer bildet med deteksjoner i samme mappe som scriptet
 
+# kjører gjennom en løkke for å skrive ut klasse og koordinater for hver deteksjon
 for boks in result[0].boxes:
     klasse = result[0].names[int(boks.cls[0])]
     koordinater = boks.xyxy[0].tolist()
---
+++ yolo-objektdeteksjon/del2_webkamera/kamera_deteksjon.py
@@ -0,0 +1,61 @@
+import cv2
+import time 
+from ultralytics import YOLO
+
+
+def kamera_deteksjon():
+    try:
+        modell = YOLO("best.pt")          # laster ned automatisk første gang
+        kamera = cv2.VideoCapture(0) # 0 for innebygd kamera, 1 for ekstern
+        forrige_tid = time.time()
+
+        while True:
+            suksess, frame = kamera.read()
+            if not suksess:
+                print("Feil ved innlesning av kamera")
+                break
+            
+            resultater = modell(frame, verbose=False)
+            annotert = resultater[0].plot()
+            fps = 1 / (time.time() - forrige_tid)
+
+            forrige_tid = time.time()
+            cv2.putText(annotert, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow("Deteksjon", annotert)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Avslutter deteksjon")
+                break
+    except Exception as e:
+        print(f"En feil oppsto: {e}")
+    finally:
+        kamera.release()
+        cv2.destroyAllWindows()
+
+
+kamera_deteksjon()
+
+
+
+"""
+modell = YOLO("best.pt")          # laster ned automatisk første gang
+kamera = cv2.VideoCapture(0) # 0 for innebygd kamera, 1 for ekstern
+forrige_tid = time.time()
+while True:
+    suksess, frame = kamera.read()
+    if not suksess:
+        print("feil ved innlesning av kamera")
+        break
+    resultater = modell(frame, verbose=False)
+    annotert = resultater[0].plot()
+
+    fps = 1 / (time.time() - forrige_tid)
+    forrige_tid = time.time()
+    cv2.putText(annotert, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    cv2.imshow("Deteksjon", annotert)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+kamera.release()
+cv2.destroyAllWindows()
+"""
