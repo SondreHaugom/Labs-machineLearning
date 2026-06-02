6225337b470f28a4ff38b203516008f335e59539
Sondre Haugom <sondrehaugom@gmail.com>
2026-06-02 13:27:30 +0200

    La til bilde deteksjon i app.py. skal bygge mer funksjonalitet rundt det å legge til utvalgte bilder selv

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from kamera_deteksjon import kamera_deteksjon
 import csv
 from datetime import datetime
 import inquirer
-
+from ultralytics import YOLO
 
 valg_liste = [
     "Start live deteksjon",
@@ -10,7 +10,26 @@ valg_liste = [
     "Avslutt"
 ]
 
-"""
+
+def bilde_deteksjon():
+    model = YOLO("best.pt")
+
+    result = model(
+        "yolo-objektdeteksjon/del1_grunnlegende/20260528_133629.jpg",
+        conf=0.40, # minimun confidence for å beholde deteksjonen
+        iou=0.20, # 
+        max_det=10, # maks antall deteksjoner per bilde
+        agnostic_nms=True, # ikke skille mellom klasser ved NMS
+    )
+
+    result[0].save("Resultat.jpg") # lagrer bildet med deteksjoner i samme mappe som scriptet
+
+    for boks in result[0].boxes:
+        klasse = result[0].names[int(boks.cls[0])]
+        koordinater = boks.xyxy[0].tolist()
+        logg_deteksjon("deteksjoner.csv", klasse, boks.conf[0], koordinater)
+        print(f"Klasse: {klasse}, Koordinater: {koordinater}")
+
 
 
 def logg_deteksjon(fil, klasse, konfidens, boks):
@@ -22,7 +41,6 @@ def logg_deteksjon(fil, klasse, konfidens, boks):
              *[int(v) for v in boks]
         ])
 
-"""
 
 
 
@@ -31,10 +49,12 @@ if __name__ == "__main__":
         userInput = inquirer.list_input("Velg et alternativ:", choices=valg_liste)
         if userInput == "Start live deteksjon":
             print("Starter live deteksjon...")
+            logg_deteksjon("deteksjoner.csv", "Live Deteksjon", 0.0, [0, 0, 0, 0]) # Logg for live deteksjon
             kamera_deteksjon()
           
         elif userInput == "Legg til bilde for deteksjon":
-            print("Funksjonalitet for bildeopplasting er under utvikling.")
+            print("Legger til bilde for deteksjon...")
+            bilde_deteksjon()
 
 
         elif userInput == "Avslutt":
